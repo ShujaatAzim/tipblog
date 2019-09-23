217cde770457cacbe2d5f8c1d9acd4423598244f
Shujaat Azim <shujaat.azim@gmail.com>
2019-09-23 12:50:04 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    total: "",
+    service: "",
+    partySize: "",
+    totalTip: "",
+    tipPP: ""
+  }
+
+  handleTotal = (event) => {
+    this.setState({
+      total: event.target.value
+    })
+  }
+
+  handleService = (event) => {
+    this.setState({
+      service: event.target.value
+    })
+  }
+
+  handleParty = (event) => {
+    this.setState({
+      partySize: event.target.value
+    })
+  }
+
+  genTip = (event) => {
+    event.preventDefault();
+    let tip = parseFloat(this.state.total * this.state.service).toFixed(2)
+    let totalTipPP = parseFloat(tip / this.state.partySize).toFixed(2)
+    this.setState({
+      totalTip: tip,
+      tipPP: totalTipPP
+    })
+  }
+
+
+  render() {
+    return (
+      <div style={{ backgroundColor: "whitesmoke"}}>
+        <h1>My Tip App</h1>
+        <form onSubmit={this.genTip}>
+          <label>
+            Bill total:
+            <input required type="text" placeholder="$" value={this.state.total} onChange={this.handleTotal} />
+          </label>
+          <label>
+            Service:
+            <input required type="text" placeholder="%" value={this.state.service} onChange={this.handleService} />
+          </label>
+          <label>
+            Party Size:
+            <input required type="text" placeholder ="#" value={this.state.partySize} onChange={this.handleParty} />
+          </label>
+          <input type="submit" />
+        </form>
+        <p>Tip: ${this.state.totalTip}</p>
+        <p>Per person: ${this.state.tipPP}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
